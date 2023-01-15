b3c7eda9834ab3d79a253fe2713e74e21981635a
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-01-15 15:23:15 +0100

    fix: made changes to source code

--- NugetPackage/Operation.cs
+++ NugetPackage/Operation.cs
@@ -11,12 +11,12 @@ namespace NugetPackage
         
         public int Subtraction(int num1, int num2)
         {
-            return num1 + num2;
+            return num1 - num2;
         }
         
         public int Division(int num1, int num2)
         {
-            return num1 + num2;
+            return num1 / num2;
         }
         
         public int Multiplication(int num1, int num2)
