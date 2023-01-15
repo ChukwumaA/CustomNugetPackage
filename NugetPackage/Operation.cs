dec5c23039a4a1ef83601c44171783f00f0e667e
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-01-15 14:50:26 +0100

    fix: removed the Console.WriteLine calls.

--- NugetPackage/Operation.cs
+++ NugetPackage/Operation.cs
@@ -6,31 +6,26 @@ namespace NugetPackage
     {
         public int Addition(int num1, int num2)
         {
-            Console.WriteLine(num1 + num2);
             return num1 + num2;
         }
         
         public int Subtraction(int num1, int num2)
         {
-            Console.WriteLine(num1 - num2);
             return num1 + num2;
         }
         
         public int Division(int num1, int num2)
         {
-            Console.WriteLine(num1 / num2);
             return num1 + num2;
         }
         
         public int Multiplication(int num1, int num2)
         {
-            Console.WriteLine(num1 * num2);
             return num1 * num2;
         }
         
         public int PrintNumbers(int digit)
         {
-            Console.WriteLine(digit);
             return digit;
         }
     }
